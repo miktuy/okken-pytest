3271ec624375325665d3f9a8970f6b33bf64963a
miktuy <miktuy@mail.ru>
2023-02-16 22:51:05 +0400

    chapter 6: marking

--- chapter6/reg/test_start.py
+++ chapter6/reg/test_start.py
@@ -2,6 +2,7 @@ import pytest
 from cards import Card, InvalidCardId
 
 
+@pytest.mark.smoke
 def test_start(cards_db):
     i = cards_db.add_card(Card("foo", state="todo"))
     cards_db.start(i)
@@ -9,6 +10,7 @@ def test_start(cards_db):
     assert c.state == "in prog"
 
 
+@pytest.mark.exception
 def test_start_non_existent(cards_db):
     any_number = 123
     with pytest.raises(InvalidCardId):
